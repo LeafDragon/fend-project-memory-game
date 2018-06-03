9c08552df560e169fbeebbf3ef6e10f5353ef18a
Frank Dip <example@example.com>
2018-06-02 22:26:47 -0700

    Added card click flip
    Added the toggler for the card classes.

--- js/app.js
+++ js/app.js
@@ -70,3 +70,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ deckUL.addEventListener("click", function(event) {
+   if (event.target.nodeName.toLowerCase() === "li") {
+     event.target.classList.toggle("open");
+     event.target.classList.toggle("show");
+   }
+ });
